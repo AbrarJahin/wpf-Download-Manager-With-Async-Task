f240e7f1e9de1d6e1404e3d9eb060769151835d6
S. M. Abrar Jahin <abrarjahin@live.com>
2016-11-18 20:11:43 +0600

    Counting is not working perfectly - Need to implement background

--- Source Downloader App/MainWindow.xaml.cs
+++ Source Downloader App/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Gu.Wpf.Adorners;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Source_Downloader_App
 {
@@ -21,6 +12,8 @@ namespace Source_Downloader_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly String downloadFileLocation = AppDomain.CurrentDomain.BaseDirectory + "download.txt";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,7 +24,48 @@ namespace Source_Downloader_App
             downloadProgress.Visibility = Visibility.Visible;
             calculationResultTitle.Visibility = Visibility.Visible;
             calculationResultValue.Visibility = Visibility.Visible;
-            MessageBox.Show(downloadUrl.Text);
+
+            startDownloadFile(downloadUrl.Text);
+
+            downloadProgress.Visibility = Visibility.Hidden;
+        }
+
+        private async void startDownloadFile(string url)
+        {
+            //var html = new System.Net.WebClient().DownloadString(url);
+            //Console.WriteLine("Done");
+            WebClient client = new WebClient();
+
+            try
+            {
+                client.DownloadFileAsync(new Uri(url), downloadFileLocation);
+                await viewTotalDivCount(downloadFileLocation);
+                MessageBox.Show("Download Completed");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private Task viewTotalDivCount(string htmlFileLocation)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(htmlFileLocation))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String downloadedHtml = sr.ReadToEnd();
+                    var matches = Regex.Matches(downloadedHtml, "<div>");
+                    calculationResultValue.Text = matches.Count.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return Task.FromResult<object>(null);
         }
     }
 }
